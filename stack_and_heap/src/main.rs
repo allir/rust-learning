0ab1d137be9a5cc87f52e2f186d7ac0e67e0ffdd
Aðalsteinn Rúnarsson <adalsteinn.runarsson@netapp.com>
2021-03-11 14:06:54 +0000

    More on stack & heap

--- stack_and_heap/src/main.rs
+++ stack_and_heap/src/main.rs
@@ -3,6 +3,10 @@
 
 use std::mem;
 
+fn type_of<T>(_: &T) -> &'static str{
+    std::any::type_name::<T>()
+}
+
 struct Point {
     x: f64,
     y: f64
@@ -14,20 +18,25 @@ fn origin() -> Point {
 
 pub fn stack_and_heap(){
     // stack allocated
+    // Rust "stack allocates" by default
     let p1 = origin();
 
     // heap allocated
+    // in Rust you can allocate memory on the heap with the Box<T> type
     let p2 = Box::new(origin());
 
     println!("p1 takes up {} bytes", mem::size_of_val(&p1));
     println!("p2 takes up {} bytes", mem::size_of_val(&p2));
 
     // "unboxing" - reallocate from the heap to the stack
+    // after this p2 can not be used again as the value has moved to p3
     let p3 = *p2;
-    println!("p2 takes up {} bytes", mem::size_of_val(&p3));
+    println!("p3 takes up {} bytes", mem::size_of_val(&p3));
 
+    // Does not work anymore as p2 has been moved
+    //println!("{}", p2.x)
 }
 
 fn main() {
-    stack_and_heap();    
+    stack_and_heap();
 }
